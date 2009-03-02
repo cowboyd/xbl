d1b6193c9370beae52b78ab5fc0c5e28287bbd2b
Charles Lowell <cowboyd@thefrontside.net>
2009-03-02 08:58:50 -0500

    add copyright notice to the xbox library

--- src/xbl/error/NotSignedInException.java
+++ src/xbl/error/NotSignedInException.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2009. The Frontside Software, Inc.
+ *
+ * The contents of this file are subject to the Gnu General Public License version 2
+ * or later (the "License"); You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
 package xbl.error;
 
 public class NotSignedInException extends RuntimeException {
